1cd442c696007700800b187cd7c502b94e12aa3a
Kael-at-471112739454 <kael@example.com>
2024-09-27 09:19:29 +0800

    Initial commit

---
+++ test/progressive-overload-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProgressiveOverloadBackend from '../lib/progressive-overload-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/progressive-overload-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProgressiveOverloadBackend.ProgressiveOverloadBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
